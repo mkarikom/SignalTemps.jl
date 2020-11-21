f77026bba522a2fbabde8e31d70da06237f3aa40
Matt Karikomi <mattkarikomi@gmail.com>
2020-11-21 11:27:26 -0800

    wip

--- src/SignalTemps.jl
+++ src/SignalTemps.jl
@@ -1,5 +1,13 @@
 module SignalTemps
 
-# Write your package code here.
+using CUDA, Distributed
+using LinearAlgebra
+using DataFrames,CSV
+using StatsBase, Distributions, Random
 
+export getTemps
+
+include("temps.jl") # calculate interactions
+
+include("data.jl") # load in the data
 end
---
+++ src/data.jl
@@ -0,0 +1,7 @@
+# load the LR interactions from a file
+function loadLR(fname::String)
+end
+
+# load the receptor/pathway genes from a file
+function loadRP(fname::String)
+end
