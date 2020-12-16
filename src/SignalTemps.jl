cc3115f6da8bd72e1ae1c8bdb1f3cdd087018f5f
Matt Karikomi <mattkarikomi@gmail.com>
2020-12-15 23:25:06 -0800

    wip

--- src/SignalTemps.jl
+++ src/SignalTemps.jl
@@ -5,17 +5,15 @@ using LinearAlgebra
 using DataFrames,Query
 using StatsBase, Distributions, Random
 using UMAP
-using LightGraphs,MetaGraphs,PCquery
+using LightGraphs,MetaGraphs
 
 export getalpha, getbeta, getkappa, gettemps, safediv, getup, getupavg, groupavg, getgroups
 export filterdf,filteravg,filtersum,umapexpr,sublabels,fplot,filterexpressed
-export enumerateLRT,lrtDist,lrtManual
+export lrtManual
 
 include("temps.jl") # calculate interactions
 
 include("data.jl") # load in the data
-
-include("lrtGraph.jl") # pcquery graph based lrt ident
 
 include("lrtManual.jl") # manual lrt ident, uses OrthoDB ortholog (eg danio) search from PCquery given primary (eg human) pathway genes
 end
